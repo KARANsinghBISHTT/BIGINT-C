248c9da591078e4761b1629b68e750c6fec04cb4
KARAN SINGH BISHT <100745824+KARANsinghBISHTT@users.noreply.github.com>
2023-12-24 20:00:01 +0530

    Create bigint.h

---
+++ bigint.h
@@ -0,0 +1,15 @@
+#ifndef BIGINT_H_
+#define BIGINT_H_
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+char* bigadd(char str1[], char str2[]);
+char* bigsubs(char str1[], char str2[]);
+char* bigmulti(char str1[], char str2[]);
+char* bigdiv(char str1[], char str2[]);
+
+
+
+#endif
